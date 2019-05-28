34102ed3a733e833d0a8e120f071bd401af42c71
wilsoniain <wilsoniain@live.com>
2019-05-28 20:09:25 +0100

    delete

--- src/loop.java
+++
@@ -1,8 +0,0 @@
-public class loop {
-  public static void main(String[] args) {
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda", "VWVW"};
-    for (String i : cars) {
-      System.out.println(i);
-    }    
-  }
-}
